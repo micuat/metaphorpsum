6415c0c6a437b332cba09ade62c363e4570d787b
micuat <micuat@gmail.com>
2021-11-19 15:21:15 +0900

    simple

--- app.js
+++ app.js
@@ -1,42 +1,106 @@
-// -----------------------------------------------
-//   LOREM METAPHORPSUM: a generator of metaphor
-// -----------------------------------------------
-// created by Kyle Stetz -> kylestetz@gmail.com
-// and released under the MIT license.
-// -----------------------------------------------
+var Sentencer = require('sentencer');
+var randy = require('randy');
+var Readable = require('stream').Readable;
 
-var express = require('express')
-  , routes = require('./routes')
-  , indexRoutes = require('./routes/index')
-  , http = require('http')
-  , path = require('path')
-  , cors = require('cors');
-
-var app = express();
-
-// all environments
-app.set('port', process.env.PORT || 3000);
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-app.use(express.favicon('public/favicon.ico'));
-// you'll want to uncomment this if you're developing locally.
-// app.use(express.logger('dev'));
-app.use(cors());
-app.use(express.bodyParser());
-app.use(express.methodOverride());
-  app.use(express.cookieParser('metaphorpsum'));
-  app.use(express.session());
-app.use(app.router);
-  app.use(require('less-middleware')(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'public')));
-
-// development only
-if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
+// generate sentences synchronously...
+// useful for the homepage.
+function generate(numberOfSentences) {
+  var sentences = "";
+  var i = 0;
+  for(i; i < numberOfSentences; i++) {
+    sentences += capitalizeFirstLetter( randomStartingPhrase() + makeSentenceFromTemplate()) + ".";
+    sentences += (numberOfSentences > 1) ? " " : "";
+  }
+  return sentences;
 }
 
-require('./routes')(app);
+// generate one sentence at a time
+// for use in a stream.
+function generateSentenceForStream(last) {
+  // make a sentence. perhaps it has a starting phrase.
+  var phrase = randomStartingPhrase();
+  var sentence = capitalizeFirstLetter( phrase + makeSentenceFromTemplate() ) + ".";
+  // add a space if it's not the last one
+  sentence += ((last) ? "" : " ");
+  return sentence;
+}
 
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
-});
+// ----------------------------------------------------------------------
+//                      TEMPLATES & CONVENIENCE F()s
+// ----------------------------------------------------------------------
+
+function makeSentenceFromTemplate() {
+  return Sentencer.make( randy.choice(sentenceTemplates) );
+}
+
+function capitalizeFirstLetter(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+// returns a starting phrase about a third of the time, otherwise it's empty
+function randomStartingPhrase() {
+  if(Math.random() < 0.33) {
+    return randy.choice(phrases);
+  }
+  return "";
+}
+
+// style guide: no periods, no first capital letters.
+var sentenceTemplates = [
+  "the {{ noun }} is {{ a_noun }}",
+  "{{ a_noun }} is {{ an_adjective }} {{ noun }}",
+  "the first {{ adjective }} {{ noun }} is, in its own way, {{ a_noun }}",
+  "their {{ noun }} was, in this moment, {{ an_adjective }} {{ noun }}",
+  "{{ a_noun }} is {{ a_noun }} from the right perspective",
+  "the literature would have us believe that {{ an_adjective }} {{ noun }} is not but {{ a_noun }}",
+  "{{ an_adjective }} {{ noun }} is {{ a_noun }} of the mind",
+  "the {{ adjective }} {{ noun }} reveals itself as {{ an_adjective }} {{ noun }} to those who look",
+  "authors often misinterpret the {{ noun }} as {{ an_adjective }} {{ noun }}, when in actuality it feels more like {{ an_adjective}} {{ noun }}",
+  "we can assume that any instance of {{ a_noun }} can be construed as {{ an_adjective }} {{ noun }}",
+  "they were lost without the {{ adjective }} {{ noun }} that composed their {{ noun }}",
+  "the {{ adjective }} {{ noun }} comes from {{ an_adjective }} {{ noun }}",
+  "{{ a_noun }} can hardly be considered {{ an_adjective }} {{ noun }} without also being {{ a_noun }}",
+  "few can name {{ an_adjective }} {{ noun }} that isn't {{ an_adjective }} {{ noun }}",
+  "some posit the {{ adjective }} {{ noun }} to be less than {{ adjective }}",
+  "{{ a_noun }} of the {{ noun }} is assumed to be {{ an_adjective }} {{ noun }}",
+  "{{ a_noun }} sees {{ a_noun }} as {{ an_adjective }} {{ noun }}",
+  "the {{ noun }} of {{ a_noun }} becomes {{ an_adjective }} {{ noun }}",
+  "{{ a_noun }} is {{ a_noun }}'s {{ noun }}",
+  "{{ a_noun }} is the {{ noun }} of {{ a_noun }}",
+  "{{ an_adjective }} {{ noun }}'s {{ noun }} comes with it the thought that the {{ adjective }} {{ noun }} is {{ a_noun }}",
+  "{{ nouns }} are {{ adjective }} {{ nouns }}",
+  "{{ adjective }} {{ nouns }} show us how {{ nouns }} can be {{ nouns }}",
+  "before {{ nouns }}, {{ nouns }} were only {{ nouns }}",
+  "those {{ nouns }} are nothing more than {{ nouns }}",
+  "some {{ adjective }} {{ nouns }} are thought of simply as {{ nouns }}",
+  "one cannot separate {{ nouns }} from {{ adjective }} {{ nouns }}",
+  "the {{ nouns }} could be said to resemble {{ adjective }} {{ nouns }}",
+  "{{ an_adjective }} {{ noun }} without {{ nouns }} is truly a {{ noun }} of {{ adjective }} {{ nouns }}"
+];
+
+// partial phrases to start with. Capitalized.
+var phrases = [
+  "To be more specific, ",
+  "In recent years, ",
+  "However, ",
+  "Some assert that ",
+  "If this was somewhat unclear, ",
+  "Unfortunately, that is wrong; on the contrary, ",
+  "This could be, or perhaps ",
+  "This is not to discredit the idea that ",
+  "We know that ",
+  "It's an undeniable fact, really; ",
+  "Framed in a different way, ",
+  "What we don't know for sure is whether or not ",
+  "As far as we can estimate, ",
+  "The zeitgeist contends that ",
+  "Though we assume the latter, ",
+  "Far from the truth, ",
+  "Extending this logic, ",
+  "Nowhere is it disputed that ",
+  "In modern times ",
+  "In ancient times ",
+  "Recent controversy aside, "
+];
+
+console.log(generate(4))
